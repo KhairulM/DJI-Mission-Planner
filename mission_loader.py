350aee1e23fffd91379229996a77238a8d4fdc00
Khairul Makirin <khairul240999@gmail.com>
2022-02-10 15:47:15 +0700

    remove rth update mission config example

--- mission_loader.py
+++ mission_loader.py
@@ -70,7 +70,6 @@ class MissionLoader:
                         Mission(MissionType.right, [rackSize[rack]["width"] / 2 + rackSize[rack+1]["width"]/2]))
 
         # rth after finishing missions
-        missions.append(Mission(MissionType.rth))
         missions.append(Mission(MissionType.land))
 
         if (self.verbose):
